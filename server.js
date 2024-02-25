bcb95e0456c37c73a622991a872bdef1b7f9256b
Kari <kari_martikainen@icloud.com>
2024-02-25 23:03:38 +0200

    puhelinluettelon backend step2 valmis

--- server.js
+++ server.js
@@ -16,11 +16,24 @@ let numbers = [
         id: 3,
         name: "Juha Martikainen",
         number: "0491281199",
+    },
+    {
+        id: 4,
+        name: "Jaakko Jokinen",
+        number: "0419249277",
     }
 ]
 
 app.get("/", (request, response) => {
     response.send("<h1>Puhelinluettelo</h1>")
+})
+
+app.get("/info", (request, response) => {
+    const summa = numbers.length > 0
+    ? Math.max(...numbers.map(n => n.id))
+    : 0
+    const requestDateTime = new Date().toLocaleString('fi-FI', { timeZone: 'Europe/Helsinki' });
+    response.send(`Phonebook has info for ${summa} people\n Request made at: ${requestDateTime}`)
 })
 
 app.get("/api/persons", (request, response) => {
