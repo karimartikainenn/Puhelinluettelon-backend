c2fa3f07e0f13314d9a8be19ce7871cc88a0f139
Kari <kari_martikainen@icloud.com>
2024-02-26 22:02:18 +0200

    ?

--- server.js
+++ server.js
@@ -19,15 +19,6 @@ morgan.token('postData', (request) => {
   return '';
 });
 
-// Database connection
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error.message);
-  });
-
 app.get('/', (request, response) => {
   response.send('<h1>Puhelinluettelo</h1>');
 });
