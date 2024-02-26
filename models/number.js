c47cdd20a287ccc96e126ed8230b5a772c6c87f3
Kari <kari_martikainen@icloud.com>
2024-02-26 22:47:34 +0200

    validator

--- models/number.js
+++ models/number.js
@@ -20,7 +20,33 @@ const numberSchema = new mongoose.Schema({
     minLength: 3,
     required: true,
   },
-  number: String,
+  number: {
+   type: String,
+   minLength: 8,
+   number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: function(v) {
+        const parts = v.split('-');
+        if (parts.length !== 2) {
+          return false;
+        }
+
+        if (!/^\d{2,3}$/.test(parts[0])) {
+          return false;
+        }
+  
+        if (!/^\d+$/.test(parts[1])) {
+          return false;
+        }
+  
+        return true;
+      },
+      message: props => `${props.value} ei ole kelvollinen puhelinnumero!`
+    },
+    required: [true, 'Puhelinnumero vaaditaan']
+  }
 });
 
 numberSchema.set("toJSON", {
